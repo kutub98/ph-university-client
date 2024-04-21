63519d74c1e11223bb9e74d40ef43b62c3eff482
Kutub Udd <socialnetwork98710@gmail.com>
2024-04-21 18:47:13 +0600

    added mainlayout ui

--- src/Components/Layouts/MainLayout.tsx
+++ src/Components/Layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Children, useState } from 'react';
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
@@ -10,6 +10,34 @@ import { Button, Layout, Menu, theme } from 'antd';
 
 const { Header, Sider, Content } = Layout;
 
+const Items = [
+  {
+    key: '1',
+    label: 'Profile',
+  },
+  {
+    key: '2',
+    label: 'User',
+  },
+  {
+    key: '3',
+    label: 'Create user',
+    children: [
+      {
+        key: '1.1',
+        label: 'Create Admin',
+      },
+      {
+        key: '1.2',
+        label: 'Create Faculty',
+      },
+      {
+        key: '1.3',
+        label: 'Create User',
+      },
+    ],
+  },
+];
 const MainLayout: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
   const {
@@ -17,58 +45,68 @@ const MainLayout: React.FC = () => {
   } = theme.useToken();
 
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
+    <>
+      <div
+        className=""
+        style={{
+          width: '98%',
+          margin:"auto",
+          background: 'rgb(19, 1, 60)',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          color: 'white',
+          padding: '8px 12px',
+          top: '0',
+          left: '0',
+          position: 'sticky',
+          zIndex: '999',
+        }}
+      >
+        <h1>PH-UNIVERSITY</h1>
+        <Button>LOG OUT</Button>
+      </div>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
+        <Sider
+          style={{ height: '100vh', position: 'static' }}
+          trigger={null}
+          collapsible
+          collapsed={collapsed}
         >
-          Content will be Here
-        </Content>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={Items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </>
   );
 };
 
