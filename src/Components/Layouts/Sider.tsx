3833434216b60202acb0554c8b30756fe2defa2c
Kutub Udd <socialnetwork98710@gmail.com>
2024-04-22 22:26:25 +0600

    added baseApi, authSlice, reducer of setUser, logout

--- src/Components/Layouts/Sider.tsx
+++ src/Components/Layouts/Sider.tsx
@@ -1,23 +1,45 @@
-
-const {  Sider } = Layout;
-import {  Layout, Menu, } from 'antd';
+const { Sider } = Layout;
+import { Layout, Menu } from 'antd';
 import { sidebarGenerator } from '../../Utils/sidebarGenerator';
 import { adminPath } from '../../Routes/admin.router';
-const SiderBar = ({collapsed}: {collapsed: boolean}) => {
+import { facultyPath } from '../../Routes/faculty.router';
+import { studentPath } from '../../Routes/student.route';
+
+const userRole = {
+  ADMIN: 'admin',
+  FACULTY: 'faculty',
+  STUDENT: 'student',
+};
+const SiderBar = ({ collapsed }: { collapsed: boolean }) => {
+  const role = 'faculty';
+  let sidebarItems;
+
+  switch (role) {
+    case userRole.ADMIN:
+      sidebarItems = sidebarGenerator(adminPath, userRole.ADMIN);
+      break;
+    case userRole.FACULTY:
+      sidebarItems = sidebarGenerator(facultyPath, userRole.FACULTY);
+      break;
+    case userRole.STUDENT:
+      sidebarItems = sidebarGenerator(studentPath, userRole.STUDENT);
+      break;
+  }
+
   return (
     <Sider
-          style={{ height: '100vh', position: 'static' }}
-          trigger={null}
-          collapsible
-          collapsed={collapsed}
-        >
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            items={sidebarGenerator(adminPath, 'admin')}
-          />
-        </Sider>
+      style={{ height: '100vh', position: 'static' }}
+      trigger={null}
+      collapsible
+      collapsed={collapsed}
+    >
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        items={sidebarItems}
+      />
+    </Sider>
   );
 };
 
---
+++ src/Redux/auth/authSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type TInitialState = {
+  user: null | object;
+  token: null | string;
+};
+
+const initialState: TInitialState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      (state.token = token), (state.user = user);
+    },
+    logOut: state => {
+      (state.user = null), (state.token = null);
+    },
+  },
+});
+
+export const { setUser, logOut } = authSlice.actions;
+export default authSlice.reducer;
